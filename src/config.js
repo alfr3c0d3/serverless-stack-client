30d3918d3f872614fade614c2bc4902edadffed1
Alfredo Morales <alfr3dit0.dev@gmail.com>
2021-01-26 18:11:55 -0500

    Changed Identity pool

--- src/config.js
+++ src/config.js
@@ -11,7 +11,7 @@ const config = {
     REGION: 'us-east-1',
     USER_POOL_ID: 'us-east-1_9hNiLVM2s',
     APP_CLIENT_ID: '6o13979ktlosqddcc96uca81rm',
-    IDENTITY_POOL_ID: 'us-east-1:652b75d1-d008-45de-94a9-3163967c2e7a',
+    IDENTITY_POOL_ID: 'us-east-1:2de77502-1cb7-4daf-87a3-cc925b1e0290',
   },
   MAX_ATTACHMENT_SIZE: 5000000,
 };
