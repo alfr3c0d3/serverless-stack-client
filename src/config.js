3be6d910755e9c036593ed6aa6896d8a097316e1
Alfredo Morales <alfr3dit0.dev@gmail.com>
2021-01-26 17:24:32 -0500

    Adding Changes

--- src/config.js
+++ src/config.js
@@ -5,13 +5,13 @@ const config = {
   },
   apiGateway: {
     REGION: 'us-east-1',
-    URL: 'https://dfqhvoidr9.execute-api.us-east-1.amazonaws.com',
+    URL: 'https://dfqhvoidr9.execute-api.us-east-1.amazonaws.com/prod',
   },
   cognito: {
     REGION: 'us-east-1',
     USER_POOL_ID: 'us-east-1_9hNiLVM2s',
     APP_CLIENT_ID: '6o13979ktlosqddcc96uca81rm',
-    IDENTITY_POOL_ID: 'us-east-1:bdeaef5b-cec4-4b38-b7e9-cb22a8bfde73',
+    IDENTITY_POOL_ID: 'us-east-1:652b75d1-d008-45de-94a9-3163967c2e7a',
   },
   MAX_ATTACHMENT_SIZE: 5000000,
 };
